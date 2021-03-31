f2722a01945e5f8f4676d7ea6ca2e91180258be7
chandrakanthajanith <chandrakanthajnaith@gmail.com>
2021-03-31 15:58:31 +0530

    first prooject

---
+++ codefest2021/src/main/java/com/codefest/springc/Codefest2021Application.java
@@ -0,0 +1,13 @@
+package com.codefest.springc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codefest2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Codefest2021Application.class, args);
+	}
+
+}
---
+++ codefest2021/src/test/java/com/codefest/springc/Codefest2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefest.springc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codefest2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
